8164b33deaa1e112f0188cdeabfae48a5ae590e1
Pedro <71204668+sprayx@users.noreply.github.com>
2022-01-14 16:14:37 -0300

    Update block.js

--- block.js
+++ block.js
@@ -1,13 +1,16 @@
 window.onkeydown = function() {
-   var key = event.keyCode || event.charCode || e.which;
-   if(key==17){ alert('stay mad.'); return false; }
-   
+    var key = event.keyCode || event.charCode || e.which;
+    if (key == 17) {
+        alert('stay mad.');
+        return false;
+    }
+
 }
 
 window.onload = function() {
 
-            document.oncontextmenu = function() {
-                window.open("https://omg.sprayx.dev")
-                return false;
-            }
-        }
+    document.oncontextmenu = function() {
+        window.open("https://omg.sprayx.dev")
+        return false;
+    }
+}

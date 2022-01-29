500753bf27b5a9bca605380e6ab5c925fe36b263
Pedro <71204668+sprayx@users.noreply.github.com>
2022-01-28 22:13:06 -0300

    Update block.js

--- block.js
+++ block.js
@@ -1,7 +1,10 @@
+//Script de bloquear control shift i ou o botao direito do mouse, retirei do meu site
+
 window.onkeydown = function() {
     var key = event.keyCode || event.charCode || e.which;
     if (key == 17) {
-        alert('stay mad.');
+        alert("É proibido inspecionar a página.")
+        //window.open("https://omg.sprayx.dev")
         return false;
     }
 
@@ -10,7 +13,8 @@ window.onkeydown = function() {
 window.onload = function() {
 
     document.oncontextmenu = function() {
-        window.open("https://omg.sprayx.dev")
+        alert("É proibido inspecionar a página.")
+        //window.open("https://omg.sprayx.dev")
         return false;
     }
 }
